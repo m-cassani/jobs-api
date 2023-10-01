79fb75ff8afa7f624294c05f1082adfc2d688349
Marcelo Cassani <marcelo.cassani@outlook.com>
2023-09-30 21:29:25 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"github.com/m-cassani/jobs-api/config"
 	"github.com/m-cassani/jobs-api/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
-	// Initialize router
+
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
